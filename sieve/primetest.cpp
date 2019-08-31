ab1104c812970d2f150b48d10fbb86a459abe5e7
Mahmud Ridwan <m@hjr265.me>
2019-08-31 13:57:11 +0600

    Create primetest.cpp

---
+++ sieve/primetest.cpp
@@ -0,0 +1,10 @@
+    bool isPrime = true ; 
+     
+    int c = sqrt( N ) 
+     
+    for( int i = 2 ; i <= c ; i ++ ) {
+            if( (N % i) == 0 ) {
+                    isPrime = false ; 
+            } 
+    }
+    
